b9f7aa9ceef08e9eef24753d0ad3d9faa689322e
Hamza Ouggadi <hamza.ouggadi@gmail.com>
2023-07-11 15:30:08 +0100

    Initial commit

---
+++ src/main/java/net/hamzaouggadi/Main.java
@@ -0,0 +1,13 @@
+package net.hamzaouggadi;
+
+
+import net.hamzaouggadi.jabir.Window;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Window window = Window.get();
+        window.run();
+
+    }
+}
